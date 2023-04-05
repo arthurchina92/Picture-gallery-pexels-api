dd317d30d642b2e2a6b13092879f20f5e51221e3
arthurchina92 <arthurchinaa@gmail.com>
2023-04-04 18:55:49 -0600

    Created and organised navigation stack screens

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { HomeScreen } from './src/screens/HomeScreen';
+import { ImageScreen } from './src/screens/ImageScreen';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Homescreen' component={HomeScreen}/>
+        <Stack.Screen name='ImageScreen' component={ImageScreen}/>
+      </Stack.Navigator>
+      <StatusBar/>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+function HomeScreen() {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+export {HomeScreen};
